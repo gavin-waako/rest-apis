c5f7c2202621dadcd6fd7ba66d25393d4704bcca
Gavin Waako <gavin.sonny27@gmail.com>
2026-03-02 12:57:19 -0500

    Connect backend to frontend

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ app.use(bodyParser.json());
 
 app.use("/api", router);
 
+// Specify folder with html files to serve as static assets
+app.use(express.static("client"));
+
 let PORT = 1234;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
